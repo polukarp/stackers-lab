153ce52f83458770e3b34070d8dc4207f02a945f
Danil Perzhul <perzhul.works@gmail.com>
2024-03-19 21:49:43 +0300

    feat: main page structure

--- music-app/src/App.tsx
+++ music-app/src/App.tsx
@@ -1,3 +1,47 @@
+import { useEffect, useState } from "react";
+import mockData from "./mock.json";
+import type { Album, Track, MockData } from "@/shared";
+
 export const App: React.FC = () => {
-  return <></>;
+  const [data, setData] = useState<MockData | null>(null);
+
+  useEffect(() => {
+    if (mockData) {
+      setData(mockData);
+    }
+  }, [mockData]);
+
+  if (!data?.albums) return <div>No albums available</div>;
+
+  return (
+    <div className="flex flex-col">
+      {data.albums.map((album) => (
+        <Album data={album} />
+      ))}
+    </div>
+  );
+};
+
+const Album: React.FC<{ data: Album }> = ({ data }) => {
+  const { tracks, title } = data;
+
+  return (
+    <div className="flex flex-col gap-2 items-center justify-center">
+      <h2 className="">{title}</h2>
+      <div className="flex flex-col gap-2">
+        {tracks.map(({ title, duration }) => (
+          <Track title={title} duration={duration} />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+const Track: React.FC<Partial<Track>> = ({ title, duration }) => {
+  return (
+    <div className="flex flex-col gap-2">
+      <div>{title}</div>
+      <div>{duration}</div>
+    </div>
+  );
 };
